5f944d91d85c5fdbab8c7daa4d92a573c70146ed
PlayeurZero <playeurzero@gmail.com>
2016-09-17 14:15:09 +0200

    Setting up default components

---
+++ server/app.js
@@ -0,0 +1,24 @@
+"use strict";
+
+let Sharebox = require('core/sharebox');
+
+let sharebox_options = {
+	socket_server : {
+		port : 8080
+	},
+	
+	http_server : {
+		port : 80,
+		folders : {
+			client : 'client',
+			download : 'server/downloads'
+		},
+		compression : {
+			enabled : true,
+			methods : ['deflate', 'gzip']
+		}
+	}
+};
+
+let sharebox = new Sharebox(sharebox_options);
+sharebox.run();
